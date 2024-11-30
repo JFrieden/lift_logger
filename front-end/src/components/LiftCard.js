1924d3c8ff85316e7881063fd9043e5e452ddcb0
JFrieden <jfrieden19@gmail.com>
2024-11-30 13:42:03 -0600

    Basic Styling Updates For Homepage

--- front-end/src/components/LiftCard.js
+++ front-end/src/components/LiftCard.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "../styles/LiftCard.css";
+import { BsThreeDots } from "react-icons/bs";
 
 const LiftCard = ({ lift, onClick }) => {
 	return (
 		<div className="lift-card" onClick={onClick} key={lift.key}>
-			<h2 className="lift-name">{lift.name}</h2>
-			<p className="lift-date">
-				{new Date(lift.date).toISOString().slice(0, 10)}
-			</p>
+			<div>
+				<h2 className="lift-name">{lift.name}</h2>
+				<p className="lift-date">
+					{new Date(lift.date).toISOString().slice(0, 10)}
+				</p>
+			</div>
+			<div className="three-dots-container">
+				<BsThreeDots style={{ fontSize: "1.5em" }} />
+			</div>
 		</div>
 	);
 };
