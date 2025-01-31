a543f52527c51289bca842b5ef52567b0c9d21d6
JFrieden <58039818+JFrieden@users.noreply.github.com>
2025-01-30 22:50:46 -0600

    Session management (#24)
    * Massive improvements all around
    
    * Fixed Form Submit Behavior
    
    * Complex Sets
    
    * Handling Bugs in ExerciseModal
    
    * Improved ExerciseModal mobile scroll
    
    * Backend Package Updates
    
    * Backend Session Refresh Updates
    
    * public dir updates
    
    * Frontend Session Refresh Updates
    
    * Bug fixes
    
    * Linting and bug fixes

--- front-end/src/axios_instance.js
+++ front-end/src/axios_instance.js
@@ -3,16 +3,11 @@ import axios from "axios";
 const baseURL =
 	process.env.NODE_ENV === "production"
 		? "https://lift-logger-4b08aa94a99a.herokuapp.com/"
-		: "http://192.168.0.21:5000";
+		: "http://localhost:5000";
 
 const instance = axios.create({
 	baseURL: baseURL,
-});
-
-instance.interceptors.request.use((config) => {
-	const token = localStorage.getItem("token");
-	if (token) config.headers.Authorization = `Bearer ${token}`;
-	return config;
+	withCredentials: true,
 });
 
 export default instance;
