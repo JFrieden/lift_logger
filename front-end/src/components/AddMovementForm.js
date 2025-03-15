09254c76d8612717a80c8c22f4f85f56a6f94165
JFrieden <jfrieden19@gmail.com>
2025-03-15 15:43:35 -0500

    Added order checking to lift logs on new log added

--- front-end/src/components/AddMovementForm.js
+++ front-end/src/components/AddMovementForm.js
@@ -115,8 +115,8 @@ const AddMovementForm = ({ liftId, onMovementAdded }) => {
 	const handleSubmitLog = async (log) => {
 		if (formData.selectedMovement) {
 			// TODO: There's a cheaper way to get the order than this.
-			const liftLen =
-				(await axios.get(`lift_logs/${liftId}`)).data.length + 1;
+			const maxOrder = (await axios.put(`lift_logs/order/${liftId}`)).data
+				.maxOrder;
 
 			const newLiftLog = {
 				movement_id: log.movement_id,
@@ -126,7 +126,7 @@ const AddMovementForm = ({ liftId, onMovementAdded }) => {
 				reps: log.reps,
 				weight: log.weight,
 				notes: log.notes,
-				order: liftLen,
+				order: maxOrder + 1,
 			};
 			const response = await axios.post("/lift_logs", newLiftLog);
 			onMovementAdded(response.data.lift_log);
